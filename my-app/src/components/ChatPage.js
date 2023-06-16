7bf88b6b7fb51c627edceb2fcb893dd28066da12
bdauria1 <103322615+bdauria1@users.noreply.github.com>
2023-06-16 13:03:12 -0400

    formatting the chatpage correctly

--- my-app/src/components/ChatPage.js
+++ my-app/src/components/ChatPage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 const ChatPage = () => {
   const [chats, setChats] = useState([]);
+  const [userInput, setUserInput] = useState('');
 
   // Fetch chat data from an API or any data source
   useEffect(() => {
@@ -12,21 +13,47 @@ const ChatPage = () => {
     // setChats(fetchedChats);
   }, []);
 
+  const handleUserInput = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const handleUserSubmit = (event) => {
+    event.preventDefault();
+    // Handle user's question submission
+    // Example:
+    // const newChat = createNewChat(userInput);
+    // setChats([...chats, newChat]);
+    setUserInput('');
+  };
+
   return (
-    <div className="chat-page">
-      <div className="chat-sidebar">
-        <h2>New Chats</h2>
-        <ul className="chat-list">
-          {chats.map(chat => (
-            <li key={chat.id}>
-              <Link to={`/chats/${chat.id}`}>{chat.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="chat-container">
-        {/* Render the chat content based on the selected chat */}
-        {/* Example: <ChatContent chatId={selectedChatId} /> */}
+    <div className="chat-container">
+      <div className="chat-page">
+        <div className="chat-sidebar">
+          <h2>New Chats</h2>
+          <ul className="chat-list">
+            {chats.map((chat) => (
+              <li key={chat.id}>
+                <Link to={`/chats/${chat.id}`}>{chat.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="chat-content">
+          {/* Render the chat content based on the selected chat */}
+          {/* Example: <ChatContent chatId={selectedChatId} /> */}
+        </div>
+        <form className="user-input" onSubmit={handleUserSubmit}>
+          <div className="input-container">
+            <input
+              type="text"
+              value={userInput}
+              onChange={handleUserInput}
+              placeholder="Type your question here"
+            />
+            <button type="submit">></button>
+          </div>
+        </form>
       </div>
     </div>
   );
