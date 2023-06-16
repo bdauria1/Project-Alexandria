4c42c4b7f94402bff457d68dbe7b14f6e8009298
bdauria1 <103322615+bdauria1@users.noreply.github.com>
2023-06-15 23:10:36 -0400

    redoing chatpage

--- my-app/src/components/ChatPage.js
+++ my-app/src/components/ChatPage.js
@@ -1,76 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import backgroundImage from './img/photo-1522661067900-ab829854a57f.avif';
 
 const ChatPage = () => {
-  const [messages, setMessages] = useState([]);
-  const [inputText, setInputText] = useState('');
+  const [chats, setChats] = useState([]);
 
-  const handleInputChange = (event) => {
-    setInputText(event.target.value);
-  };
-
-  const handleSendMessage = () => {
-    if (inputText.trim() !== '') {
-      const newMessage = {
-        id: messages.length + 1,
-        text: inputText,
-        sender: 'user',
-      };
-
-      setMessages([...messages, newMessage]);
-      setInputText('');
-    }
-  };
-
+  // Fetch chat data from an API or any data source
   useEffect(() => {
-
-    document.body.style.backgroundImage = `url(${backgroundImage})`;
-    document.body.style.backgroundRepeat = 'no-repeat';
-    document.body.style.backgroundPosition = 'top left, top right';
-    document.body.style.backgroundSize = 'cover';
-
-    return () => {
-      document.body.style.backgroundImage = '';
-      document.body.style.backgroundRepeat = '';
-      document.body.style.backgroundPosition = '';
-      document.body.style.backgroundSize = '';
-    };
+    // Fetch the chat data and update the 'chats' state
+    // Example:
+    // const fetchedChats = fetchChats();
+    // setChats(fetchedChats);
   }, []);
 
   return (
-    <div className="chat-container">
-      <header>
-        <nav>
-          <ul>
-            <li><Link to="/categories">Categories</Link></li>
-            <li><Link to="/services">Services</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
-      </header>
-      <div className="contacts-container">
-        <div className="contacts-box"></div>
+    <div className="chat-page">
+      <div className="chat-sidebar">
+        <h2>New Chats</h2>
+        <ul className="chat-list">
+          {chats.map(chat => (
+            <li key={chat.id}>
+              <Link to={`/chats/${chat.id}`}>{chat.title}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
-      <div className="chat-messages">
-        {messages.map((message) => (
-          <div
-            key={message.id}
-            className={`message ${message.sender === 'user' ? 'user-message' : 'ai-message'}`}
-          >
-            {message.text}
-          </div>
-        ))}
-      </div>
-      <div className="chat-input">
-        <input
-          type="text"
-          placeholder="Type your message..."
-          value={inputText}
-          onChange={handleInputChange}
-        />
-        <button onClick={handleSendMessage}>Send</button>
+      <div className="chat-container">
+        {/* Render the chat content based on the selected chat */}
+        {/* Example: <ChatContent chatId={selectedChatId} /> */}
       </div>
     </div>
   );
