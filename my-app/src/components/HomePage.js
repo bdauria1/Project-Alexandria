f43f644ce213e4baa8207eb1f505fa8c39fbf6c9
bdauria1 <103322615+bdauria1@users.noreply.github.com>
2023-06-22 01:36:25 -0400

    redid home page

--- my-app/src/components/HomePage.js
+++ my-app/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import tutorial from './img/image123.png';
+import image1 from './img/imageomg.png';
 
 const HomePage = () => {
   const handleLogin = () => {
@@ -11,13 +12,13 @@ const HomePage = () => {
     <div className="homepage">
       <header>
         <nav>
-          <div className='men'><a><Link to="/categories">Menu</Link></a></div>
-          <div className='log'><Link to="/login">Login/Signup</Link></div>
+          <a><Link to="/categories">Menu</Link></a>
+          <a><Link to="/login">Login/Signup</Link></a>
         </nav>
       </header>
       <div className='pagebody'>
         <div className='welcome'>
-
+        <img src={image1} className="welcomeimage"/>
         </div>
         <div className='tutorial'>
           <h1>How it Works</h1>
@@ -30,7 +31,32 @@ const HomePage = () => {
         </div>
         <div className='about'>
           <h1>About Us</h1>
-          <h2></h2>
+          <div className='aboutinfo'>
+            <div className='whitepaper'>
+              <h2>Whitepaper</h2>
+              <p>
+                Our mission is to create a market economy for knowledge and 
+                information monetization. Everyone should be able to
+                make an income by selling their expertise.
+              </p>
+            </div>
+            <div className='deck'>
+              <h2>Deck</h2>
+              <p>
+                Our mission is to create a market economy for knowledge and information monetization. 
+                Everyone should be able to make an income by selling their expertise.
+              </p>
+            </div>
+            <div className='ourstaff'>
+              <h2>Our Staff</h2>
+              <p>
+                Sorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                Sorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Etiam eu turpis molestie, dictum est a, mattis tellus. 
+              </p>
+            </div>
+          </div>
         </div>
         <div className='questions'>
           <h1>Have any questions?</h1>
@@ -39,8 +65,33 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-      <footer className="footer-bottom">
-        <p>&copy; 2023 Alexandria. All rights reserved.</p>
+      <footer>
+        <div className='listings'>
+          <div className='collection'>
+            <h3>collection</h3>
+            <ul>
+              <li><a>Home</a></li>
+              <li><a>Categories</a></li>
+              <li><a>How it Works</a></li>
+            </ul>
+          </div>
+          <div className='relizations'>
+            <h3>relizations</h3>
+            <ul>
+              <li><a>Home</a></li>
+              <li><a>Categories</a></li>
+              <li><a>How it Works</a></li>
+            </ul>
+          </div>
+          <div className='contacts'>
+            <h3>contacts</h3>
+            <ul>
+              <li><a>Home</a></li>
+              <li><a>Categories</a></li>
+              <li><a>How it Works</a></li>
+            </ul>
+          </div>
+        </div>
       </footer>
     </div>
   );
